d21cc220bda6421362ff9f558f6c06788c121661
ToastarN <oskar231@hotmail.com>
2020-10-05 20:27:53 +0200

    Added mysql connection

--- inspektor/settings.py
+++ inspektor/settings.py
@@ -82,12 +82,18 @@ WSGI_APPLICATION = 'inspektor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'inspektor',
+        'USER': 'dbadmin',
+        'PASSWORD': 'oskar',
+        'HOST': '192.168.64.2',
+        'PORT': '3306'
     }
 }
 
 
+#"""'ENGINE': 'django.db.backends.sqlite3',
+#'NAME': BASE_DIR / 'db.sqlite3',"""
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
