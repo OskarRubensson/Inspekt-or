bc3b95c24cae9cc914d3112251acd0e65ebb1622
ToastarN <oskar231@hotmail.com>
2020-12-23 00:15:59 +0100

    Still hate migrations

--- inspektor/urls.py
+++ inspektor/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from accounts import views as acc_views
+from userInfo import views as acc_views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
